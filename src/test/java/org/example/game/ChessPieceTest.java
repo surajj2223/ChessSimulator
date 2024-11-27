6593070eafd200782f4681c1527a29a506b92292
Suraj Malviya <23surajj95@gmail.com>
2024-11-27 23:39:36 +0530

    chore: adds pawn edge case test

--- src/test/java/org/example/game/ChessPieceTest.java
+++ src/test/java/org/example/game/ChessPieceTest.java
@@ -15,10 +15,19 @@ public class ChessPieceTest {
         ChessPiece pawn = new Pawn();
         List<String> possibleMoves = pawn.getPossibleMoves("D5");
 
-        // Expected move for a Pawn at D5 (one step forward)
+        // Only expected move for a Pawn at D5 (one step forward)
         List<String> expectedMoves = List.of("D6");
 
         assertEquals(expectedMoves.size(), possibleMoves.size());
         assertTrue(possibleMoves.containsAll(expectedMoves));
+    }
+
+    @Test
+    void testPawnMovesEdge() {
+        ChessPiece pawn = new Pawn();
+        List<String> possibleMoves = pawn.getPossibleMoves("D8");
+
+        assertEquals(0, possibleMoves.size());
+        assertTrue(possibleMoves.isEmpty());
     }
 }
